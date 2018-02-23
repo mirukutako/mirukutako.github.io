b743af59bbe1e1c84e0817fa32f33f9ee079f5a4
Malash Dzmitrij <d.malash@flex.by>
2018-02-23 14:44:01 +0300

    fix menu

--- umc/js/main.js
+++ umc/js/main.js
@@ -5,15 +5,30 @@ $('.js-mob-menu').off('click touchstart').on('click ', function(e){
     $('html').toggleClass('menu-active');
 })
 
-$('.mob-menu .wrap-list').off('click ').on('click touchstart', function(e){
-    if($(this).hasClass('visible')){
-        $(this).removeClass('visible');
-        return false;
-    } else {
-        $(this).addClass('visible').siblings().removeClass('visible');
-        return false;
-    }
-});
+var mob_menu = $('.mob-menu .wrap-list');
+mob_menu.each(function(){
+    var _t = $(this);
+    _t.off('click ').on('click', function(e){
+        var second = _t.find('.second-level'),
+            second_item = second.find('li');
+        if(_t.hasClass('visible')){
+            if(second.is(e.target) || second_item.is(e.target)){
+                console.log('qq')
+                return false;
+            } else {
+                console.log('dd')
+                _t.removeClass('visible');
+                return false;
+            }
+            _t.removeClass('visible');
+            return false;
+        } else {
+            _t.addClass('visible').siblings().removeClass('visible');
+            return false;
+        }
+    });
+})
+// $('.mob-menu .wrap-list')
 //new funcrtion
 
 
